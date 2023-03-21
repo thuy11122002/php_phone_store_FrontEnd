6700cba7db79e725c2bd1f9e08776f86d2a39a80
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-21 23:38:39 +0700

    Created Route for admin panel

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,37 @@
-<?php require("templates/admin-home.php");
+<?php
+        $url_requested = $_SERVER['REQUEST_URI'];
+        $url_len = strlen($url_requested);
+        
+        $actual_path = substr($url_requested,strripos($url_requested,'/')); 
+        
+        if($actual_path == "/"){
+            $page_title = "Home";
+            require("./templates/admin-home.php");
+           
+        }    
+        if($actual_path == "/user"){
+            $page_title = "user";
+            require("./templates/admin-user-management.php");
+           
+        } 
+        if($actual_path == "/product"){
+            $page_title = "product";
+            require("./templates/admin-product-management.php");
+           
+        } 
+        if($actual_path == "/order"){
+            $page_title = "order";
+            require("./templates/admin-order-management.php");
+            
+        } 
+        if($actual_path == "/brand"){
+            $page_title = "brand";
+            require("./templates/admin-brand-management.php");
+           
+        } 
+        if($actual_path == "/inventory"){
+            $page_title = "inventory";
+            require("./templates/admin-inventory-management.php");
+           
+        } 
+        
