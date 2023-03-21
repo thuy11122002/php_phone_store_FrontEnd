0336e9f7a01c841ae272286c7391b4d1b805ecd0
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-21 15:53:45 +0700

    added sidebar menu

--- admin/includes/admin-header.php
+++ admin/includes/admin-header.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <a class="navbar-brand" style="padding-left: 25px" href="#">Admin</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
---
+++ admin/includes/admin-side-nav.php
@@ -0,0 +1,19 @@
+
+    <div class="row">
+        <!-- Menu Section -->
+        <div class="col-3" id="sticky-sidebar">
+            <div class="sticky-top" style="padding-top:55px">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action active">
+                      Dashboard
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">Users</a>
+                    <a href="#" class="list-group-item list-group-item-action">Products</a>
+                    <a href="#" class="list-group-item list-group-item-action">Brands</a>
+                    <a href="#" class="list-group-item list-group-item-action ">Orders</a>
+                    <a href="#" class="list-group-item list-group-item-action ">Inventory</a>
+                  </div>
+            </div>
+        </div>
+
+        <!-- Content Section -->
