30ad19e55ffed715b7bb9223a758c351e4ff2cc7
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-24 19:13:25 +0700

    add control to user table

--- admin/includes/content/user-management.php
+++ admin/includes/content/user-management.php
@@ -6,19 +6,21 @@
                 <div class="col">
                     <div class="input-group">
                         <input type="search" name="search-bar" class="form-control" placeholder="Search user"
-                            aria-label="Recipient's username" aria-describedby="button-addon1">
-                        <button class="btn btn-outline-secondary" name="button1" type="submit-button" id="button1">Search</button>
+                            aria-label="Username" aria-describedby="button-addon1">
+                        <button class="btn btn-outline-secondary" name="button1" type="submit" id="button1">Search</button>
                     </div>
                 </div>
                 <div class="col">
-                    <button class="btn btn-primary" name="button2" type="submit-button" id="button2">Add user</button>
+                    <button class="btn btn-primary" name="button2" type="button" id="button2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Add user</button>
                 </div>
             </div>
         </form>
         <?php
-         require("./includes/scripts/user-management-form-handler.php");
-         require("./includes/componement/tables/table-user-list.php");
+         require("./includes/scripts/handlers/user-management-form-handler.php");
+         require_once("./includes/componement/tables/table-user-list.php");
+         require("./includes/componement/forms/form-add-user.php");
         ?>
+        
     </div>
 </div>
 
