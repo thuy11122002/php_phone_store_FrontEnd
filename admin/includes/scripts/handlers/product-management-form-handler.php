7ca00f4f76961e5e8f7530a96c12e311f7e8c5c4
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-26 02:29:33 +0700

    add scripts to product-management and re-stryling

--- admin/includes/scripts/handlers/product-management-form-handler.php
+++ admin/includes/scripts/handlers/product-management-form-handler.php
@@ -2,14 +2,22 @@
 
         function testfun()
         {
-            echo $_POST['search-bar'];
+            echo $_POST['button'];
         }
 
         if (array_key_exists('button1', $_POST)) {
             testfun();
         }
-        if (array_key_exists('button2', $_POST)) {
-            echo "add product btn clicked";
+        if (array_key_exists('button3', $_POST)) {
+            require_once("./includes/scripts/api/postProduct.php");
+            $data=$_POST;
+            unset($data['button3']);
+            $data['productID']=4;
+            var_dump(json_decode($data));
+            echo '<br>';
+            var_dump(json_decode($_POST));
+           // postProduct(json_encode($data));
         }
+        
 
         ?>
