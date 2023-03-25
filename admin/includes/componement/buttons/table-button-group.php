b10d00d79771ce05999c891042ae08425eb624f7
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-26 02:26:53 +0700

    Created button group to be used inside a table

---
+++ admin/includes/componement/buttons/table-button-group.php
@@ -0,0 +1,5 @@
+<div class="btn-group sm" role="group" aria-label="User Table Button">
+  <button type="button" class="btn btn-outline-primary btn-sm">View</button>
+  <button type="button" class="btn btn-outline-info btn-sm">Edit</button>
+  <button type="button" class="btn btn-outline-danger btn-sm">Delete</button>
+</div>
