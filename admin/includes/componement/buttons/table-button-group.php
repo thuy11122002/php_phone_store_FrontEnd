ce714dacefd44d8b8fda1399b12f0a7d6d31e267
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-27 23:43:30 +0700

    Update table-button-group

--- admin/includes/componement/buttons/table-button-group.php
+++ admin/includes/componement/buttons/table-button-group.php
@@ -1,5 +1,7 @@
+
 <div class="btn-group sm" role="group" aria-label="User Table Button">
-  <button type="button" class="btn btn-outline-primary btn-sm">View</button>
-  <button type="button" class="btn btn-outline-info btn-sm">Edit</button>
-  <button type="button" class="btn btn-outline-danger btn-sm">Delete</button>
+  <button type="submit" name="view" class="btn btn-outline-primary btn-sm">View</button>
+  <button type="submit" name="edit"  class="btn btn-outline-info btn-sm">Edit</button>
+  <button type="submit" name="delete" class="btn btn-outline-danger btn-sm">Delete</button>
 </div>
+
