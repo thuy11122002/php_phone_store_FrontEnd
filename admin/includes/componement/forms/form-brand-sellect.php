7dadb59f1003b22e92d6f81581fa3319fbdf2ca5
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-27 21:48:21 +0700

    Update form-select-brand in add-product-form

--- admin/includes/componement/forms/form-brand-sellect.php
+++ admin/includes/componement/forms/form-brand-sellect.php
@@ -1,5 +1,5 @@
 <label for="select-brand" class="form-label">Brand</label>
-<select class="form-select form-select mb-3" name="brand" aria-label="Select brand" id="select-brand">
+<select class="form-select form-select mb-3" name="brandID" aria-label="Select brand" id="select-brand">
     <option selected>Select Brand</option>
     <?php 
         require_once("./includes/scripts/api/getBrand.php");
