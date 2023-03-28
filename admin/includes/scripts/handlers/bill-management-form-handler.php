b099c9c79179ffaf13decb3231a8161b65e64bc5
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2023-03-28 16:49:31 +0700

    Bill management Read and Delete operation

---
+++ admin/includes/scripts/handlers/bill-management-form-handler.php
@@ -0,0 +1,34 @@
+<?php
+        require_once("./includes/scripts/api/APIs.php");
+        function search()
+        {
+            echo $_POST['search-bar'];
+        }
+        function viewItem(){
+            
+            echo  $_POST['rowID'];
+        }
+        function deleteItem(){
+            $url= 'http://localhost:8080/bill/'.''.$_POST['rowID'];
+            deleteAPI($url);
+        }
+        function editItem(){
+            
+        }
+
+        if (array_key_exists('button1', $_POST)) {
+            search();
+        }
+        if (array_key_exists('button2', $_POST)) {
+            echo "add user btn clicked";
+        }
+        if (array_key_exists('view', $_POST)) {
+            viewItem();
+        }
+        if (array_key_exists('edit', $_POST)) {
+            editItem();
+        }
+        if (array_key_exists('delete', $_POST)) {
+           deleteItem();
+        }
+        ?>
