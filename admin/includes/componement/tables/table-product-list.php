20e70b74156e868ba26039c6fdb4b73bef571c61
cmi-nguyen <67179509+cmi-nguyen@users.noreply.github.com>
2023-04-18 10:22:53 +0700

    update user table and product table

--- admin/includes/componement/tables/table-product-list.php
+++ admin/includes/componement/tables/table-product-list.php
@@ -13,39 +13,45 @@
         </tr>
     </thead>
     <tbody>
-        <?php 
-          require_once("./includes/scripts/api/APIs.php");
-          $url1='http://localhost:8080/product';
-          $url2='http://localhost:8080/brand';
+        <?php
+        require_once("./includes/scripts/api/APIs.php");
+        $url1 = 'http://localhost:8080/product';
+        $url2 = 'http://localhost:8080/brand';
 
-          $resp = getList($url1);
-          $brands = getList($url2);
-          
-          foreach($resp as $rs){
+        $resp = getList($url1);
+        $brands = getList($url2);
+
+        foreach ($resp as $rs) {
             echo '<tr>';
-            echo '<th scope="row">'.$rs->productID.'</th>';
-            echo '<td>'.$rs->productName.'</td>';
-            echo '<td>'.$rs->price.'</td>';
-            foreach($brands as $br){
-                if($rs->brandID==$br->brandID){
-                    echo '<td>'.$br->brandName.'</td>';
+            echo '<th scope="row">' . $rs->productID . '</th>';
+            echo '<td>' . $rs->productName . '</td>';
+            echo '<td>' . $rs->price . '</td>';
+            foreach ($brands as $br) {
+                if ($rs->brandID == $br->brandID) {
+                    echo '<td>' . $br->brandName . '</td>';
                 }
             }
-            echo '<td>'.$rs->stock.'</td>';
-            echo '<td>'.$rs->imgURL.'</td>';
-            echo '<td>'.$rs->description.'</td>';
-            
-            
-            
-            echo  '<td>';
-            //require("./includes/componement/buttons/table-button-group.php");
+            echo '<td>' . $rs->stock . '</td>';
+            echo '<td>' . $rs->imgURL . '</td>';
+            echo '<td>' . $rs->description . '</td>';
+
+
+
+            echo '<td>';
+            echo '<div class="d-flex">';
+            echo '<form method="get">';
+            echo '<input type="hidden" name="rowID" value="' . strval($rs->productID) . '">';
+            echo '<button type="submit" name="view" class="btn btn-outline-primary btn-sm">View</button>
+            <button type="submit" name="edit"  class="btn btn-outline-info btn-sm">Edit</button>';
+            echo '</form>';
             echo '<form method="post">';
             echo '<input type="hidden" name="rowID" value="' . strval($rs->productID) . '">';
-            require("./includes/componement/buttons/table-button-group.php");
+            echo '<button type="submit" name="delete" class="btn btn-outline-danger btn-sm">Delete</button>';
             echo '</form>';
+            echo '</div>';
             echo '</td>';
             echo '</tr>';
-          }
+        }
         ?>
     </tbody>
 
